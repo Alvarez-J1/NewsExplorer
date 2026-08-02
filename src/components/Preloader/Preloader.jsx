c2a8fa6fdb548cf54bb90a04ec361a3d3de0cc04
Joel Alvarez <Joel124@outlook.es>
2026-08-02 00:45:23 -0400

    a11y: announce search loading state

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,7 @@ import "./Preloader.css";
 
 export default function Preloader() {
   return (
-    <section className="results-loading">
+    <section className="results-loading" role="status" aria-live="polite">
       <div className="preloader">
         <div className="circle-preloader" aria-hidden="true" />
         <p className="preloader__text">Searching for news...</p>

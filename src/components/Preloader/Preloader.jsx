bb23750143993488198e2076e503c80c8bfbb7a8
Joel Alvarez <Joel124@outlook.es>
2026-08-30 06:28:16 -0400

    a11y: mark loading status as busy

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -2,7 +2,12 @@ import "./Preloader.css";
 
 export default function Preloader() {
   return (
-    <section className="results-loading" role="status" aria-live="polite">
+    <section
+      className="results-loading"
+      role="status"
+      aria-live="polite"
+      aria-busy="true"
+    >
       <div className="preloader">
         <div className="circle-preloader" aria-hidden="true" />
         <p className="preloader__text">Searching for news...</p>
